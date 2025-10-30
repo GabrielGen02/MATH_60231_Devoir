477e9fa1de1e518ad09b7fa83bc0454573d28cee
GabrielGen02 <gabriel.2.gendron@hec.ca>
2025-10-30 15:54:29 -0400

    Update Devoir_1_Gab.py

--- Devoir_1_Gab.py
+++ Devoir_1_Gab.py
@@ -1,8 +1,47 @@
 
+# importation des packages nécessaires pour le devoir 
+import wrds
+import pandas as pd
 
-x = 1 
-x + 2 
-print("test")
+# connexion a un compte wrds pour pouvoir télécharger les données requises pour le devoir 
+db = wrds.Connection()
 
-z = x**2
-z
+# fonction permettant de créer un df contenant : date, permno, prix, rendement quotidien et le ticker
+def get_prices(db, tickers, start_date='2012-01-03', end_date='2023-01-03'):
+    """
+    Extrait les prix quotidiens pour une liste de tickers entre deux dates.
+    db : connexion WRDS active
+    tickers : liste de tickers (ex: ['AAPL', 'JPM', 'XOM', 'SPY'])
+    """
+    # Étape 1 : récupérer les permnos
+    tickers_str = "', '".join(tickers)
+    query_permno = f"""
+        SELECT DISTINCT permno, ticker
+        FROM crsp.stocknames
+        WHERE ticker IN ('{tickers_str}')
+    """
+    df_permno = db.raw_sql(query_permno)
+    
+    # Étape 2 : extraire les prix
+    permnos = df_permno['permno'].tolist()
+    permnos_str = ", ".join(str(p) for p in permnos)
+    query_prices = f"""
+        SELECT date, permno, prc, ret
+        FROM crsp.dsf
+        WHERE date BETWEEN '{start_date}' AND '{end_date}'
+        AND permno IN ({permnos_str})
+    """
+    df_prices = db.raw_sql(query_prices)
+    
+    df_prices = db.raw_sql(query_prices)
+
+    # Merge pour ajouter les tickers au df final 
+    df_prices = df_prices.merge(df_permno, on='permno', how='left')
+
+
+    return df_prices
+
+# exemple de df pouvant être créé : 
+tickers_gabriel = ['SPY', 'JPM', 'AAPL', 'XOM']
+df_gabriel = get_prices(db, tickers_gabriel)
+print(df_gabriel.head())
